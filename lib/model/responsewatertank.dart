25158b79c3e257043fc74b8e93d69e9a5c0e7be5
AdrianoEdu <adrianoeduardotrentin@hotmail.com>
2023-03-29 01:29:45 -0300

    Fix: Add to images

--- lib/model/responsewatertank.dart
+++ lib/model/responsewatertank.dart
@@ -1,7 +1,7 @@
 class ResponseWaterTank {
-  double totalCapacityByLiter = 1000;
+  double totalCapacityByLiter = 0;
   double currentCapacityByLiter = 0;
-  double totalCapacityByMeter = 1;
+  double totalCapacityByMeter = 0;
   double currentCapacityByMeter = 0;
   DateTime sent = DateTime(2023);
 }
--- lib/watertankwidget.dart
+++ lib/watertankwidget.dart
@@ -16,11 +16,39 @@ class WaterTankWidget extends StatelessWidget {
         children: <Widget>[
           Text("Capacidade Total : ${resp.totalCapacityByLiter} litro"),
           Text("Capacidade Atual : ${resp.currentCapacityByLiter} litro"),
-          Text("Capacidade Total : ${resp.totalCapacityByLiter} metro"),
-          Text("Capacidade Atual : ${resp.currentCapacityByLiter}"),
-          Text("Data Envada : ${resp.sent.toString()}")
+          Text("Capacidade Total : ${resp.totalCapacityByMeter} metro"),
+          Text("Capacidade Atual : ${resp.currentCapacityByMeter} metro"),
+          Text("Data Envada : ${resp.sent.toString()}"),
+          Image.asset(getStatusGraphWaterTank(resp.currentCapacityByLiter))
         ],
       ),
     );
+  }
+
+  String getStatusGraphWaterTank(double status) {
+    switch (status.toString()) {
+      case "100.0":
+        return "images/Caixa1.png";
+      case "200.0":
+        return "images/Caixa2.png";
+      case "300.0":
+        return "images/Caixa3.png";
+      case "400.0":
+        return "images/Caixa4.png";
+      case "500.0":
+        return "images/Caixa5.png";
+      case "600.0":
+        return "images/Caixa6.png";
+      case "700.0":
+        return "images/Caixa7.png";
+      case "800.0":
+        return "images/Caixa8.png";
+      case "900.0":
+        return "images/Caixa9.png";
+      case "1000.0":
+        return "images/Caixa10.png";
+      default:
+        return "images/Caixa0.png";
+    }
   }
 }
